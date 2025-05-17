2a223afada902f12961ebceb73cb049711ce0eef
mig1023 <mig1023@mail.ru>
2025-05-17 10:13:01 +0300

    Temporary external config

--- interceraptor/CRM/Secret.cs
+++ interceraptor/CRM/Secret.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace interceraptor.CRM
 {
@@ -6,16 +7,74 @@ namespace interceraptor.CRM
     {
         public const string Version = "4.0";
 
-        private const string IP = "127.0.0.1";
+        private static string IP
+        {
+            get
+            {
+                return __TemporaryReadInDebugFile("[BASE]");
+            }
+        }
 
-        public const string AuthenticationPath = IP + "idp/login?login=string&password=string";
+        public static string AuthenticationPath
+        {
+            get
+            {
+                //return IP + "idp/login?login=string&password=string";
+                return IP + __TemporaryReadInDebugFile("[LOGIN]");
+            }
+        }
 
-        public const string EchoPath = IP + "cashdesk/echo";
+        public static string EchoPath
+        {
+            get
+            {
+                //return IP + "cashdesk/echo";
+                return IP + __TemporaryReadInDebugFile("[ECHO]");
+            }
+        }
 
-        public const string CashierPath = IP + "configurator/conf/user/";
 
-        public const string ServicesPath = IP + "dispatcher/config/?keys=";
+        public static string CashierPath
+        {
+            get
+            {
+                //return IP + "configurator/conf/user/";
+                return IP + __TemporaryReadInDebugFile("[CONFIGURATOR]");
+            }
+        }
 
-        public const string CaltulatePath = IP + "cashdesk/pay/service";
+        public static string ServicesPath
+        {
+            get
+            {
+                //return IP + "dispatcher/config/?keys=";
+                return IP + __TemporaryReadInDebugFile("[DISPATCHER]");
+            }
+        }
+
+        public static string CaltulatePath
+        {
+            get
+            {
+                //return IP + "cashdesk/pay/service";
+                return IP + __TemporaryReadInDebugFile("[PAY]");
+            }
+        }
+
+        private static string __TemporaryReadInDebugFile(string type)
+        {
+            using (var reader = new StreamReader("debug.api.txt"))
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == type)
+                        return reader.ReadLine();
+                }
+            }
+
+            return string.Empty;
+        }
     }
 }
